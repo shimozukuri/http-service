792668cc816aee64a1d7b3096d685f64f4eaf5d7
shimozukuri <whitelord228@yandex.ru>
2026-08-13 23:42:26 +0300

    fix github actions

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,7 +15,7 @@ type Config struct {
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-required:"localhost:8080"`
+	Address     string        `yaml:"address" env-default:"localhost:8082"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 	User        string        `yaml:"user" env-required:"true"`
